f09faf4d98e74e23342e6b2c24f88bb3b0efae59
xavcha03 <ch.xavier03@gmail.com>
2022-10-31 12:52:00 +0100

    test

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const http = require("http");
 const cors = require("cors");
 const { Server } = require("socket.io");
-const async_hooks = require('async_hooks');
+const async_hooks = require("async_hooks");
 
 app.use(cors()); // Add cors middleware
 
@@ -11,7 +11,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
 	cors: {
-		origin: "http://192.168.0.10:5173",
+		origin: "2a02:2788:8f6:501:a1d8:590a:82c1:c738",
 		methods: ["GET", "POST"],
 	},
 });
